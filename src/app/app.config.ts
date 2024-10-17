995feec614c906c6840791c740cb2fb11078b5c0
M-Nafi <nafi-m@gmx.de>
2024-10-17 18:06:03 +0200

    added firebase save contacts funktion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,19 @@ import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-dd412","appId":"1:66496653220:web:536773a8acd4d22a704331","storageBucket":"simple-crm-dd412.appspot.com","apiKey":"AIzaSyA6TvcqCxhSs3UAKcGqrV9u_ypARksg75k","authDomain":"simple-crm-dd412.firebaseapp.com","messagingSenderId":"66496653220"})), provideFirestore(() => getFirestore())]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'simple-crm-dd412',
+        appId: '1:66496653220:web:536773a8acd4d22a704331',
+        storageBucket: 'simple-crm-dd412.appspot.com',
+        apiKey: 'AIzaSyA6TvcqCxhSs3UAKcGqrV9u_ypARksg75k',
+        authDomain: 'simple-crm-dd412.firebaseapp.com',
+        messagingSenderId: '66496653220',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -8,12 +8,22 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { CollectionReference } from '@firebase/firestore';
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
   providers: [provideNativeDateAdapter()],
-  imports: [MatDialogModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, CommonModule, FormsModule],
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    CommonModule,
+    FormsModule,
+  ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
@@ -21,9 +31,17 @@ export class DialogAddUserComponent {
   user = new User();
   birthDate!: Date;
 
-  saveUser() {
+  constructor(private firestore: Firestore) {}
+
+  async saveUser() {
     this.user.birthDate = this.birthDate.getTime();
     console.log('Current user is', this.user);
+    try {
+      let usersCollection = collection(this.firestore, 'users');
+      let result = await addDoc(usersCollection, this.user.toJSON());
+      console.log('Adding user finished', result);
+    } catch (error) {
+      console.error('Error adding user: ', error);
+    }
   }
-
 }
